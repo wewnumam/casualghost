689f1823d2bb68796665aa6cae9e740987a5dde1
waimus <16229430+waimus@users.noreply.github.com>
2023-02-02 11:47:43 +0700

    ammo limit and reload

--- Assets/Scripts/CharacterShooting.cs
+++ Assets/Scripts/CharacterShooting.cs
@@ -1,26 +1,79 @@
+using System.Collections;
 using UnityEngine;
 
 public class CharacterShooting : MonoBehaviour {
-    [Header("Shooting Properties")]
-    public GameObject[] bulletTypes;
-    public Transform bulletSpawnPoint;
+	[Header("Shooting Properties")]
+	public GameObject[] bulletTypes;
+	public Transform bulletSpawnPoint;
 
-    private int roundsLeft = 0;
+	[Header("Weapon Properties")]
+    [SerializeField] private WeaponType currentWeaponType = WeaponType.DEFAULT;
+	[SerializeField] private int maxRound = 0;
+	[SerializeField] private int roundsLeft = 0;
+	[SerializeField] private float reloadTime = 3.0f;
 
-	// Start is called before the first frame update
 	void Start() {
-
+        WeaponSwitch(0);
 	}
 
-	// Update is called once per frame
 	void Update() {
-        // Spawn bullet projectile
-        if (Input.GetMouseButtonDown(0)) {
-            GameObject b = Instantiate(
-                bulletTypes[0],
-                bulletSpawnPoint.position,
-                bulletSpawnPoint.rotation
-            );
-        }
+		// Spawn bullet projectile
+		if (Input.GetMouseButtonDown(0)) {
+			if (roundsLeft > 0) {
+				GameObject b = Instantiate(
+					bulletTypes[(int)currentWeaponType],
+					bulletSpawnPoint.position,
+					bulletSpawnPoint.rotation
+				);
+
+				roundsLeft--;
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.R)) {
+			if (roundsLeft < maxRound) {
+                StartCoroutine(ReloadSequence(reloadTime));
+			}
+		}
+
+        WeaponSelect();
 	}
+
+	private void WeaponSwitch(int idx) {
+        currentWeaponType = bulletTypes[idx].GetComponent<Projectile>().weaponType;
+		switch (
+			bulletTypes[idx]
+				.GetComponent<Projectile>()
+				.weaponType) {
+			case WeaponType.DEFAULT:
+				maxRound = 5;
+				reloadTime = 3.0f;
+				break;
+			case WeaponType.RIFLE:
+				maxRound = 31;
+				reloadTime = 1.3f;
+				break;
+			default:
+				break;
+		}
+        roundsLeft = maxRound;
+	}
+
+    private IEnumerator ReloadSequence(float time) {
+        yield return new WaitForSeconds(time);
+        roundsLeft = maxRound;
+    }
+
+    private void WeaponSelect() {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            WeaponSwitch(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            WeaponSwitch(1);
+        }
+    }
+}
+
+public enum WeaponType {
+	DEFAULT,
+	RIFLE
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,36 +1,39 @@
 using UnityEngine;
 
 public class Projectile : MonoBehaviour {
-    [Header("Behaviour")]
-    public float flySpeed = 12.0f;
-    public float lifetime = 4.0f;
+	[Header("Bullet Type")]
+	public WeaponType weaponType = WeaponType.DEFAULT;
 
-    private Rigidbody2D bulletPhysics;
-    private float lifetimeElapsed = 0.0f;
+	[Header("Behaviour")]
+	public float flySpeed = 12.0f;
+	public float lifetime = 4.0f;
+
+	private Rigidbody2D bulletPhysics;
+	private float lifetimeElapsed = 0.0f;
 
 	// Start is called before the first frame update
 	void Start() {
-        bulletPhysics = GetComponent<Rigidbody2D>();
+		bulletPhysics = GetComponent<Rigidbody2D>();
 	}
 
-    void Update() {
-        Life();
-    }
+	void Update() {
+		Life();
+	}
 
 	void FixedUpdate() {
-        // Fly with defined speed during its lifetime
-        bulletPhysics.velocity = transform.up * flySpeed * 32.0f * Time.deltaTime;
+		// Fly with defined speed during its lifetime
+		bulletPhysics.velocity = transform.up * flySpeed * 32.0f * Time.deltaTime;
 	}
 
-    void OnCollisionEnter2D(Collision2D collision) {
-        Destroy(this.gameObject);
-    }
+	void OnCollisionEnter2D(Collision2D collision) {
+		Destroy(this.gameObject);
+	}
 
-    void Life() {
-        // Destroy projectile once it exceeds its lifetime
-        lifetimeElapsed += Time.deltaTime;
-        if (lifetimeElapsed >= lifetime) {
-            Destroy(this.gameObject);
-        }
-    }
+	void Life() {
+		// Destroy projectile once it exceeds its lifetime
+		lifetimeElapsed += Time.deltaTime;
+		if (lifetimeElapsed >= lifetime) {
+			Destroy(this.gameObject);
+		}
+	}
 }
